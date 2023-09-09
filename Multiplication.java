c2cf2d08615533b3582f6f1a78c81133f24518b3
pratikshajadhav24 <144526621+pratikshajadhav24@users.noreply.github.com>
2023-09-09 16:04:35 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+class Multiplication{
+public static void main(String[]args){
+
+Scanner s=new Scanner(System.in);
+System.out.println("enter a number");
+int num=s.nextInt();
+ for(int i=0;i<=10;i++)
+        	System.out.println(num+" * "+i+" = "+num*i);
+}
+}
